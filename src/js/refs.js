e40f0941048335457ab73aad3d2ef08e56a60811
Jul <klop03@ukr.net>
2021-08-04 23:11:57 +0300

    done

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+    return {
+        inputRef: document.getElementById('search-box'),
+        countryContainer: document.getElementById('country-info'),
+        countryListContainer: document.getElementById('country-list')
+    }
+}
